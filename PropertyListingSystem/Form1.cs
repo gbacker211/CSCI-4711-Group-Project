be6faa510a864f4568e9dd0f5bd6ab8c72518912
cevans <CEVANS26@gru.edu>
2015-10-20 09:19:33 -0400

    some comment

--- PropertyListingSystem/Form1.cs
+++ PropertyListingSystem/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using  PropertyListing;
+
 namespace PropertyListingSystem
 {
     public partial class Form1 : Form
