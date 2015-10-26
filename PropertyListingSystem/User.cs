26f7ab76b07829ab2f22d92a309e7513d7a4da16
cevans <CEVANS26@gru.edu>
2015-10-26 13:43:23 -0400

    finished up the User class

--- PropertyListingSystem/User.cs
+++ PropertyListingSystem/User.cs
@@ -6,16 +6,36 @@ using System.Threading.Tasks;
 
 namespace PropertyListingSystem
 {
+    /// <summary>
+    ///     Class User is for storing a User retrieved 
+    ///     from the database and to be used on the system.
+    /// </summary>
     class User
     {
-        /// <summary>
-        ///     class variables
-        /// </summary>
+
+        // class variables
         private string mFname;
         private string mLname;
         private int mAgent_ID;
         private string mPhone;
         private string mEmail;
+
+        /// <summary>
+        ///     constructor for class User
+        /// </summary>
+        /// <param name="aFname"></param>
+        /// <param name="aLname"></param>
+        /// <param name="aAgent_ID"></param>
+        /// <param name="aPhone"></param>
+        /// <param name="aEmail"></param>
+        public User(string aFname = "", string aLname = "", int aAgent_ID = 0, string aPhone = "", string aEmail = "")
+        {
+            Fname = aFname;
+            Lname = aLname;
+            Agent_ID = aAgent_ID;
+            Phone = aPhone;
+            Email = aEmail;
+        }
 
         /// <summary>
         ///     Property for mFname
@@ -30,16 +50,29 @@ namespace PropertyListingSystem
         /// <summary>
         ///     Property for mAgent_ID
         /// </summary>
-        public int Agent_ID { set; get; }
+        public int Agent_ID
+        {
+            set
+            {
+                if (value >= 0)
+                    mAgent_ID = value;
+                else
+                    throw new ArgumentOutOfRangeException("Error: ID cannot be a negative...");
+            }    
+            get
+            {
+                return mAgent_ID;
+            }
+        }
 
         /// <summary>
         ///     Property for mPhone
         /// </summary>
-        public string Phone { set; get; }
+        public string Phone { set { mPhone = value; } get { return mPhone; } }
 
         /// <summary>
         ///     Property for mEmail
         /// </summary>
-        public string Email { set; get; }
+        public string Email { set { mEmail = value; } get { return mEmail; } }
     }
 }
