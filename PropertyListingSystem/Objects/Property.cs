e9b8ad89d980e8bbbe4f30295291efee00649da9
cevans <CEVANS26@gru.edu>
2015-11-12 14:17:26 -0500

    Move Controller and Objects from PropertyListing to main project

---
+++ PropertyListingSystem/Objects/Property.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace PropertyListing
+{
+    [DataContract]
+    public class Property
+    {
+        [DataMember]
+        public Int32 AgentID { get; set; }
+
+        [DataMember]
+        public String Photo_Url { get; set; }
+
+        [DataMember]
+         public  string Address { get; set; }
+
+        [DataMember]
+        public string State { get; set; }
+
+        [DataMember]
+        public string Zip { get; set; }
+
+        [DataMember]
+        public  int Price { get; set; }
+
+        [DataMember]
+        public  int NumberOfRooms { get; set; }
+
+        [DataMember]
+        public  int NumberOfBaths { get; set; }
+
+        [DataMember]
+        public  string Description { get; set; }
+    }
+}
