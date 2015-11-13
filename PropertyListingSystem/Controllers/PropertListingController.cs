dbcac1fbd2a604f124cbc0d0affa9989e9dac559
gbacker211 <gbacker@gru.edu>
2015-11-12 21:31:26 -0500

    Added Add Property Functionality

--- PropertyListingSystem/Controllers/PropertListingController.cs
+++ PropertyListingSystem/Controllers/PropertListingController.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
-using PropertyListing;
 
-namespace PropertListing
+
+namespace PropertyListingSystem
 {
     public class PropertListingController
     {
-         private  string _connectionString  = String.Empty;  //Once connection to db is established place the connection string here
+        private string _connectionString = String.Empty;  //Once connection to db is established place the connection string here
 
         public PropertListingController()
         {
@@ -19,32 +19,42 @@ namespace PropertListing
         public bool AddNewProperty(Property property)
         {
             bool value = false;
-            using (SqlConnection connection = new SqlConnection(_connectionString))
+            try
             {
-                using (SqlCommand addProperty = new SqlCommand("usp_Insert_NewProperty", connection))
+                using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
-                    connection.Open();
-                    addProperty.CommandType = CommandType.StoredProcedure;
-                    addProperty.Parameters.AddWithValue("@AgentID", property.AgentID);
-                    addProperty.Parameters.AddWithValue("@Address", property.Address);
-                    addProperty.Parameters.AddWithValue("@State", property.State);
-                    addProperty.Parameters.AddWithValue("@Zip", property.Zip);
-                    addProperty.Parameters.AddWithValue("@Price", property.Price);
-                    addProperty.Parameters.AddWithValue("@NumberOfRooms", property.NumberOfRooms);
-                    addProperty.Parameters.AddWithValue("@NumberOfBaths", property.NumberOfBaths);
-                    addProperty.Parameters.AddWithValue("@Description", property.Description);
-                    addProperty.Parameters.AddWithValue("@Photo_URL", property.Photo_Url);
-                    addProperty.Parameters.Add("@ReturnValue", SqlDbType.Int).Direction = ParameterDirection.ReturnValue;
+                    using (SqlCommand addProperty = new SqlCommand("usp_Insert_NewProperty", connection))
+                    {
+                        connection.Open();
+                        addProperty.CommandType = CommandType.StoredProcedure;
+                        addProperty.Parameters.AddWithValue("@AgentID", property.AgentID);
+                        addProperty.Parameters.AddWithValue("@Address", property.Address);
+                        addProperty.Parameters.AddWithValue("@State", property.State);
+                        addProperty.Parameters.AddWithValue("@Zip", property.Zip);
+                        addProperty.Parameters.AddWithValue("@Price", property.Price);
+                        addProperty.Parameters.AddWithValue("@NumberOfRooms", property.NumberOfRooms);
+                        addProperty.Parameters.AddWithValue("@NumberOfBaths", property.NumberOfBaths);
+                        addProperty.Parameters.AddWithValue("@Description", property.Description);
+                        addProperty.Parameters.AddWithValue("@Photo_URL", property.Photo_Url);
+                        addProperty.Parameters.Add("@ReturnValue", SqlDbType.Int).Direction = ParameterDirection.ReturnValue;
 
-                    addProperty.ExecuteNonQuery();
+                        addProperty.ExecuteNonQuery();
 
-                    value = (bool) addProperty.Parameters["@ReturnValue"].Value;
-
-
+                        value = (bool)addProperty.Parameters["@ReturnValue"].Value;
 
 
+
+
+                    }
                 }
             }
+            catch (Exception)
+            {
+
+                //EAT FAILURE FOR NOW
+            }
+
+
             return value;
         }
     }
