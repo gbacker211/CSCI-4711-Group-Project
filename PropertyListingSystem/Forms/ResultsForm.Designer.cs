6f867fcd71d26a04664b8c8c9e68cc2b11ccb82a
jreis491 <jrei@gru.edu>
2015-11-16 13:40:03 -0500

    made results form

---
+++ PropertyListingSystem/Forms/ResultsForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace PropertyListingSystem.Forms
+{
+    partial class ResultsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridResults = new System.Windows.Forms.DataGridView();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.lb_results = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridResults
+            // 
+            this.dataGridResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridResults.Location = new System.Drawing.Point(60, 56);
+            this.dataGridResults.Name = "dataGridResults";
+            this.dataGridResults.Size = new System.Drawing.Size(591, 520);
+            this.dataGridResults.TabIndex = 0;
+            this.dataGridResults.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridResults_CellContentClick);
+            // 
+            // btn_back
+            // 
+            this.btn_back.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_back.Location = new System.Drawing.Point(192, 603);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(265, 62);
+            this.btn_back.TabIndex = 1;
+            this.btn_back.Text = "Back";
+            this.btn_back.UseVisualStyleBackColor = true;
+            // 
+            // lb_results
+            // 
+            this.lb_results.AutoSize = true;
+            this.lb_results.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_results.Location = new System.Drawing.Point(253, 9);
+            this.lb_results.Name = "lb_results";
+            this.lb_results.Size = new System.Drawing.Size(150, 24);
+            this.lb_results.TabIndex = 2;
+            this.lb_results.Text = "Search Results";
+            // 
+            // ResultsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 688);
+            this.Controls.Add(this.lb_results);
+            this.Controls.Add(this.btn_back);
+            this.Controls.Add(this.dataGridResults);
+            this.Name = "ResultsForm";
+            this.Text = "ResultsForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridResults;
+        private System.Windows.Forms.Button btn_back;
+        private System.Windows.Forms.Label lb_results;
+    }
+}
---
+++ PropertyListingSystem/Forms/ResultsForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PropertyListingSystem.Forms
+{
+    public partial class ResultsForm : Form
+    {
+        public ResultsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridResults_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
